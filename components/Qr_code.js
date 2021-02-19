aec4c651795dbdeec20b22fe1f938babb6ee69c6
Elisa Moilanen <moieli@hotmail.fi>
2021-02-19 08:22:50 +0200

    Permission for camera

--- components/Qr_code.js
+++ components/Qr_code.js
@@ -2,8 +2,7 @@ import { Provider as PaperProvider, BottomNavigation, Text } from 'react-native-
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { WebView } from 'react-native-webview';
 import React, { useState } from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Alert, CameraRoll } from 'react-native';
-import { Camera } from 'expo-camera';
+import { StyleSheet, Button, View, SafeAreaView, Alert  } from 'react-native';
 
 
 
@@ -11,25 +10,62 @@ function Qr_code() {
 
     const [url, setUrl] = useState("https://expo.io");
     const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [error, setError] = useState("");
+
 
     const useScanner = async () => {
 
-        const {status} = await Camera.requestPermissionsAsync();
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
 
-        if(status === 'granted'){
-            console.log("LUPA MYÖNNETTY");
+        if (status === 'granted') {
+            setHasPermission(true);
+            setError("");
+        } 
+        else if (status === 'denied') {
+            setError('Et ole antanut oikeutta käyttää kameraa! Sovellus ei voi lukea koodia, jos et ole antanut käyttöoikeutta kameralle');
         }
+    }
+
+
+    const handleBarCodeScanned = ({data}) => {
+
+        setScanned(true);
+        console.log(data);
     }
 
 
 
     return (
-        <PaperProvider>
-            <View style={{ height: 1000 }}>
-                <WebView source={{ uri: url }}
-                    style={{ marginTop: 50, height: 500 }} />
-            </View>
+
+        (hasPermission)
+            ? <PaperProvider>
+
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} />
+
+                <View style={{ height: 1000 }}>
+                    <WebView source={{ uri: url }}
+                        style={{ marginTop: 50, height: 500 }} />
+                </View>
+                <View style={styles.button}>
+                    <Button
+                        title="Skannaa QR-koodi"
+                        onPress={useScanner}
+                        color="pink"
+                    />
+                </View>
+            </PaperProvider>
+
+
+
+            : 
+            
             <View style={styles.button}>
+                { (error) 
+                ?<Text>{error} </Text>
+                :null
+                }
                 <Button
                     title="Skannaa QR-koodi"
                     onPress={useScanner}
@@ -37,7 +73,10 @@ function Qr_code() {
                 />
             </View>
 
-        </PaperProvider>
+
+
+
+
     );
 }
 
