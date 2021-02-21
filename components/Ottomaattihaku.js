844363847473d0666b1126d68c03856bb95522b0
Elisa Moilanen <moieli@hotmail.fi>
2021-02-21 18:38:04 +0200

    added automatic search feature

--- components/Ottomaattihaku.js
+++ components/Ottomaattihaku.js
@@ -1,6 +1,6 @@
 import { View } from "react-native";
-import { Provider as PaperProvider,  Text } from 'react-native-paper';
-import React, { useState } from 'react';
+import { Provider as PaperProvider, Text } from 'react-native-paper';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Button } from 'react-native';
 import * as Location from 'expo-location';
 import Automaatit from '../assets/automaatit'
@@ -8,16 +8,24 @@ import Automaatit from '../assets/automaatit'
 
 function Ottomaattihaku() {
 
-    const [ottomaattiLocation, setOttomaattiLocation] = useState([]);
-  //  const [currentLocation, setCurrentLocation] = useState([]);
-    // const [address, setAddress] = useState("");
-    // const [postcode, setPostcode] = useState("");
-    // const [city, setCity] = useState("");
+    const [ottomaatit, setOttomaatit] = useState([]);
+    const [nearestCity, setNearestCity] = useState("");
     const [distance, setDistance] = useState("");
     const [locationPermission, setLocationPermission] = useState(false);
     const [error, setError] = useState("");
 
 
+
+    useEffect(() => {
+
+        const objectDetails = Object.values(Automaatit);
+
+        setOttomaatit(ottomaatit.concat(objectDetails));
+
+    }, []);
+
+
+    // Ask permisson for use location
     const askLocationPermission = async () => {
 
         setError("");
@@ -35,39 +43,42 @@ function Ottomaattihaku() {
                 koordinaatti_LON: locationNow.coords.longitude
             }
 
-          //  setCurrentLocation(locationObject);
-
             searchOttomaatti(locationObject);
-            //console.log("Nyt ollaan, locationObject",locationObject);
-            //console.log("Nyt ollaan, locationNow",locationNow.coords.longitude,locationNow.coords.latitude);
+
         } else {
             setError("Sinun täytyy hyväksyä puhelimelle oikeus käyttää sijaintia, jotta voit etsiä automaattia");
         }
-
     }
 
 
+    // Search nearest cash automatic
     const searchOttomaatti = (locationObject) => {
 
-        const degree_LAT_km = locationObject.koordinaatti_LAT / 360 * 40000;
-        const degree_LON_km = locationObject.koordinaatti_LON / 360 * 40000;;
+        let shortestDistance = 0;
+        let shortestCity = "";
 
-        const degree_LAT_km_ottomaatti = Automaatit[Automaatit.length-3].koordinaatti_LAT / 360 * 40000;
-        const degree_LON_km_ottomaatti = Automaatit[Automaatit.length-3].koordinaatti_LON / 360 * 40000;
+        ottomaatit.map((am) => {
 
-        const a_point = degree_LAT_km - degree_LAT_km_ottomaatti;
-        const b_point = degree_LON_km - degree_LON_km_ottomaatti;
+            let degree_LAT_km = locationObject.koordinaatti_LAT / 360 * 40000;
+            let degree_LON_km = locationObject.koordinaatti_LON / 360 * 40000;;
 
-        const distancePoint = Math.sqrt((Math.pow(a_point,2)) + (Math.pow(b_point,2)));
+            let degree_LAT_km_ottomaatti = am.koordinaatti_LAT / 360 * 40000;
+            let degree_LON_km_ottomaatti = am.koordinaatti_LON / 360 * 40000;
 
+            let a_point = degree_LAT_km - degree_LAT_km_ottomaatti;
+            let b_point = degree_LON_km - degree_LON_km_ottomaatti;
 
+            let distancePoint = Math.sqrt((Math.pow(a_point, 2)) + (Math.pow(b_point, 2)));
 
-        console.log("Etäisyys",distancePoint, " Paikkakunta: ", Automaatit[Automaatit.length-3].postitoimipaikka);
-        console.log("Lat on km: ",degree_LAT_km," ja lon on km ", degree_LON_km);
-        console.log("OTTOMAATTI Lat on km: ",degree_LAT_km_ottomaatti," ja lon on km ", degree_LON_km_ottomaatti);
+            if (distancePoint < shortestDistance || shortestDistance === 0) {
+                shortestDistance = distancePoint;
+                shortestCity = am;
+            }
 
+        })
+        setDistance(shortestDistance.toFixed(2));
+        setNearestCity(shortestCity);
     }
-
 
 
     return (
@@ -75,9 +86,16 @@ function Ottomaattihaku() {
         (locationPermission)
             ? <PaperProvider>
 
-                <View>
+                {(distance)
+                    ? <View style={styles.details}>
+                        <Text>Lähin Ottoautomaatti on {distance} km:n päässä</Text>
+                        <Text style={styles.text}>Katuosoite: {nearestCity.katuosoite}</Text>
+                        <Text>Postitoimipaikka: {nearestCity.postitoimipaikka}</Text>
+                        <Text>Postinumero: {nearestCity.postinumero}</Text>
+                    </View>
+                    : null
+                }
 
-                </View>
 
                 <View style={styles.button}>
                     <Button
@@ -91,9 +109,6 @@ function Ottomaattihaku() {
 
             : <PaperProvider>
 
-                <View>
-
-                </View>
 
                 {(error)
                     ? <View style={styles.warning}>
@@ -127,6 +142,15 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         margin: 50,
         marginTop: 100
+    },
+    details: {
+        marginTop: 300,
+        alignSelf: 'center',
+        backgroundColor: "yellow",
+        padding: 15
+    },
+    text: {
+        marginTop: 20
     }
 });
 
